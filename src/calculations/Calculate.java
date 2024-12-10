d74270a608d8f392bbf5320a585dac970b31c282
jerinjoseputhussery <60233550+jerinjoseputhussery@users.noreply.github.com>
2024-12-10 16:58:42 +0000

    Repeat and Bank holidays added

--- src/calculations/Calculate.java
+++ src/calculations/Calculate.java
@@ -11,39 +11,60 @@ public class Calculate {
 		 
 		 System.out.println("Enter number of days you worked in this pay-period:");
 		 int noOfDays = scanner.nextInt(); 
-		 
-		 String shiftTime;
+		 String input;
+		 String shiftTime="";
 		 boolean isValidated;
 		 boolean firstExecution;
 		 boolean isSunday;
+		 boolean isbankHoliday;
 		 LocalTime clock_in;
 		 LocalTime clock_out;
 		 Duration todays_timings;
 		 long pay_period_minutes=0;
 		 int night_period_hours=0;
 		 long sun_period_minutes=0;
+		 long bank_period_minutes=0;
+
 
 		 
 		 for (int i = 0; i < noOfDays; i++) {
 			 //Loop for days
-			 shiftTime="";
+			 input="";
 			 todays_timings=Duration.ZERO;
 			 isValidated=false;
 			 firstExecution=true;
 			 isSunday=false;
+			 isbankHoliday=false;
 			 do{
 				 if(firstExecution) {
 					 firstExecution=false;
-					 System.out.println("(24 hours format) separated by- (eg:- 0800-1600);\t If sunday prefix sun");
+					 System.out.println("(24 hours format) separated by- (eg:- 0800-1600);\t If sunday prefix sun\t If Repeat input re");
 					 System.out.println("Enter day"+(i+1)+" clock-in and clock out timings:");
 				 }else {
 					 System.out.println("Enter the timing in proper format. See format below:");
 					 System.out.println("(24 hours format) separated by- (eg:- 0800-1600)");				 
 				 }
-				 shiftTime = scanner.nextLine(); 
+				 input = scanner.nextLine(); 
+				 if(input.equalsIgnoreCase("re") || input.equalsIgnoreCase("repeat")) {
+					 if(shiftTime.equals("")){
+						 System.out.println("Repeat doesnot exists. Enter the timing please:");
+						 input = scanner.nextLine(); 
+						 shiftTime=input;
+					 }
+				 }
+				 else					 
+					 shiftTime=input;
 				 if(shiftTime.startsWith("SUN") || shiftTime.startsWith("sun") || shiftTime.startsWith("Sun")) {
 					 shiftTime=shiftTime.substring(3);
 					 isSunday=true;
+				 }
+//				 else if(shiftTime.startsWith("BAN") || shiftTime.startsWith("ban") || shiftTime.startsWith("Ban")) {
+//					 shiftTime=shiftTime.substring(3);
+//					 isbankHoliday=true;
+//				 }
+				 else if(shiftTime.startsWith("BANK") || shiftTime.startsWith("bank") || shiftTime.startsWith("Bank")) {
+					 shiftTime=shiftTime.substring(4);
+					 isbankHoliday=true;
 				 }
 				 isValidated = Validations.checkInputs(shiftTime);					 
 			 }while(!isValidated);
@@ -71,12 +92,16 @@ public class Calculate {
 				 
 				 if(isSunday) 
 					 sun_period_minutes+=todays_timings.toMinutes();
+				 else if(isbankHoliday)
+					 bank_period_minutes+=todays_timings.toMinutes();
 				 else
 					 pay_period_minutes+=todays_timings.toMinutes();
 			 }
 			 
 		}
 		 System.out.println("BASIC: "+pay_period_minutes/60.0+" hours");
+		 if(bank_period_minutes>0)
+			 System.out.println("BANK HOL: "+bank_period_minutes/60.0+" hours");
 		 if(sun_period_minutes>0)
 			 System.out.println("SUNDAY PREM: "+sun_period_minutes/60.0+" hours");
 		 if(night_period_hours>0)
